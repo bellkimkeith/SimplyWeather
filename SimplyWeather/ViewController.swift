7dc8853268d23dffe97243b4c2eaa6149376ead9
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-01-30 23:20:24 +0800

    Initial Commit

---
+++ SimplyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimplyWeather
+//
+//  Created by Bellkim Keith Onggon on 1/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
