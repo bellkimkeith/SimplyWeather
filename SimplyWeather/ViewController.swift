32414db2d3d2e7f058617dad026a7feac672d91a
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-02-01 23:10:26 +0800

    Adjust UI and setup for interaction
    Change the textfield background color and added UI outlets. Also made initial interaction when using textfield.

--- SimplyWeather/ViewController.swift
+++ SimplyWeather/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var getCurrentLocationButton: UIButton!
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var searchButton: UIButton!
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var weatherImage: UIImageView!
+    @IBOutlet var weatherLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(textField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        
+    }
+    
 }
 
